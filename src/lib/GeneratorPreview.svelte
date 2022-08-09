f6d8e96d423350b638c7e6d4c4efa4320356ecca
g1ver <consigner0@gmail.com>
2022-08-09 16:47:47 -0700

    setup preview

---
+++ src/lib/GeneratorPreview.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import formData from "$lib/FormDataStore";
+</script>
+
+<div class="preview">
+    <h4 class="author">{$formData.author}</h4>
+    <h4 class="title">{$formData.title}</h4>
+    <h4 class="description">{$formData.description}</h4>
+    <h4 class="image-url">{$formData.imageUrl}</h4>
+    <h4 class="redirect-url">{$formData.redirectUrl}</h4>
+    <h4 class="sidebar-color">{$formData.sidebarColor}</h4>
+</div>
+
+<style>
+    .preview {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
